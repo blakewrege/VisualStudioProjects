516395a9e05357a5f7b376ebb1f5d96f203e938b
blake <blakewrege1@gmail.com>
2016-06-23 04:21:26 -0400

    fixed chat

--- FileSharingAppClient/FileSharingApp/Form1.Designer.cs
+++ FileSharingAppClient/FileSharingApp/Form1.Designer.cs
@@ -156,13 +156,13 @@
             // 
             // button1
             // 
-            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(212, 361);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 28;
             this.button1.Text = "Send";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // lblServer
             // 
